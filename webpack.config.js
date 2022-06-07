a38bced7db40d69ae67b9a80b6a095dc25071d8b
a-gehlot <andrew.gehlot@gmail.com>
2022-06-07 14:16:57 -0400

    renaming files and adding root elements using React18 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: "./src/index.jsx",
+    entry: "./src/widgets.jsx",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js"
