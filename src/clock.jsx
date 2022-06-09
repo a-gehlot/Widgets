4df9f84199b5ad61b5e71fc4b75936281bdcc2ec
a-gehlot <andrew.gehlot@gmail.com>
2022-06-08 23:07:09 -0400

    widget and styling for clock

---
+++ src/clock.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date()
+        }
+        this.tick = this.tick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Clock</h1>
+                <ul className="clockWidget">
+                    <li><span>Time:</span> <span>{this.state.time.toLocaleTimeString()} EDT</span></li>
+                    <li><span>Date:</span> <span>{this.state.time.toDateString()}</span></li>
+                </ul>
+            </div>
+        )
+    }
+
+    tick() {
+        this.setState({
+            time: new Date(),
+        })
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000)
+    }
+}
+
+export default Clock;
--- src/widgets.jsx
+++ src/widgets.jsx
@@ -1,11 +1,11 @@
 import * as ReactDOM from 'react-dom';
 import * as ReactDOMClient from 'react-dom/client';
 import React from 'react';
+import Clock from './clock'
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('root');
     const root = ReactDOMClient.createRoot(container);
-    const element = <h1>Hello, world</h1>;
-    root.render(element)
+    root.render(<Clock/>)
 })
