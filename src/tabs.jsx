07106287d57781787ce03b949dddb93f1d7d611e
a-gehlot <andrew.gehlot@gmail.com>
2022-06-13 16:00:29 -0400

    Headers that respond to click for changing tabs

---
+++ src/tabs.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+class Headers extends React.Component {
+    render() {
+        const selected = this.props.tabIdx;
+        const headers = this.props.tabs.map((pane, idx) => {
+            const title = pane.title;
+            const isSelected = selected === idx ? 'active' : '';
+            return (
+                <li
+                key={idx}
+                className={isSelected}
+                onClick={() => this.props.changeTab(idx)}>
+                    {title}
+                </li>
+            )
+        })
+        return (
+            <ul>
+                {headers}
+            </ul>
+        )
+    }
+}
+
+class Tabs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tabIdx: 0,
+        }
+    this.changeTab = this.changeTab.bind(this)
+    }
+
+    changeTab(val) {
+        this.setState({
+            tabIdx: val
+        })
+    }
+
+    render() {
+        const selectedPane = this.props.tabs[this.state.tabIdx];
+
+        return(
+            <div>
+                <Headers
+                tabs={this.props.tabs}
+                tabIdx={this.state.tabIdx}
+                changeTab={this.changeTab}
+                />
+                <div>
+                    {selectedPane.content}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Tabs;
--- src/widgets.jsx
+++ src/widgets.jsx
@@ -2,10 +2,20 @@ import * as ReactDOM from 'react-dom';
 import * as ReactDOMClient from 'react-dom/client';
 import React from 'react';
 import Clock from './clock'
+import Tabs from './tabs'
+
+const tabs = [
+    {title: "One", content: "I am the first"},
+    {title: "Two", content: "I am the second"},
+    {title: "Three", content: "I am the third"}
+]
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('root');
     const root = ReactDOMClient.createRoot(container);
-    root.render(<Clock/>)
+    root.render(<div>
+        <Clock/>
+        <Tabs tabs={tabs}/>
+    </div>)
 })
